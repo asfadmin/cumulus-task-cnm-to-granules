d97d4139eece068c855aac6000259e48548f9b04
Rohan Weeden <reweeden@alaska.edu>
2023-05-12 11:23:06 -0800

    Add deployment example

---
+++ example/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0,!= 3.14.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+
+module "cnm_to_cma_task" {
+  source = "../build/terraform"
+
+  prefix                     = var.prefix
+  lambda_subnet_ids          = local.lambda_subnet_ids
+  lambda_security_group_ids  = local.lambda_security_group_ids
+  lambda_processing_role_arn = data.terraform_remote_state.cumulus.outputs.lambda_processing_role_arn
+  tags = {
+    Deployment = "cnm_to_cma_task example"
+  }
+}
